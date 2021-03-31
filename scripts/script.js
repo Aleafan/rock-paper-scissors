42117258ac6efd204bede02bbd56503b0601ed34
Aleafan <ae.afanasov@yandex.com>
2021-03-31 19:35:36 +0300

    Correct str case in computerPlay() and write playRound() in script.js, correct <script> link in index.html

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,33 @@
 function computerPlay() {
     // Generate random integer in range (0-2)
     let randomNum = Math.floor(Math.random() * 3);
-    console.log(randomNum);
     // Associate with each generated number one of three strings and return it
     switch (randomNum) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors"
+            return "scissors";
     }
+}
+
+// Function that plays a single round
+function playRound(playerSelection, computerSelection) {
+    // Convert user's selection to lowercase
+    playerSelection = playerSelection.toLowerCase();
+    // Compare selections and return the result
+    if (playerSelection === computerSelection) {
+        return 'You have a tie!'
+    }
+    // Create an object where properties = selections and values = "weaker" selections
+    const selectionBeat = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
+    }
+    if (computerSelection === selectionBeat[playerSelection]) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
